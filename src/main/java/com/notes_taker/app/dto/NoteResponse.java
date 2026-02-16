06e7572cb9ea3feecfa3656948928594018808c2
sn-0217 <hack3r.1702@gmail.com>
2026-02-16 11:50:00 +0530

    Feature response dto (#5)
    * removed maven
    
    * added note to note request
    
    * fixed issue application.properties
    
    * comments added to response dto
    
    ---------
    
    Co-authored-by: santhu <wsl.sn.dev@gmail.com>

--- src/main/java/com/notes_taker/app/dto/NoteResponse.java
+++ src/main/java/com/notes_taker/app/dto/NoteResponse.java
@@ -4,5 +4,6 @@ import java.time.Instant;
 
 import lombok.Builder;
 
+//used to return as response from entity
 @Builder
 public record NoteResponse(Long id, String title, String content, Instant createdAt, Instant modifiedAt) {}
